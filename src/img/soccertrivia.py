edae766382b5077d599d65bc325d183b03221dc3
kodiesarkodie <58040904+kodiesarkodie@users.noreply.github.com>
2020-01-09 10:10:14 -0500

    Update soccertrivia.py

--- src/img/soccertrivia.py
+++ src/img/soccertrivia.py
@@ -1,4 +1,31 @@
+import pygame
+import random
+
 # Ask user for name to begin
-username = input("What is your name?")
+username = input("What is your name? ")
 # greet player
-print("What's up {player}? Let's start playing the Soccer Trivia game!")
+print("What's up {username}? Let's start playing the Soccer Trivia game!")
+
+
+question1 = 
+question2 =
+question3 =
+question4 =
+question5 =
+question6 =
+question7 =
+question8 =
+question9 =
+question10 =
+question11 =
+question12 =
+question13 =
+question14 =
+question15 =
+question16 =
+question17 =
+question18 =
+question19 =
+question20 =
+
+print random()
