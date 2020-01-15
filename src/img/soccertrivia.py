7579d2ba4322647c22e3e815acd0173d956a6316
kodiesarkodie <ksarkodie@ucvts.org>
2020-01-15 10:27:08 -0500

    Update soccertrivia.py

--- src/img/soccertrivia.py
+++ src/img/soccertrivia.py
@@ -1,4 +1,3 @@
-import pygame
 import random
 
 # Ask user for name to begin
@@ -6,26 +5,31 @@ username = input("What is your name? ")
 # greet player
 print("What's up {username}? Let's start playing the Soccer Trivia game!")
 
+# set starting score for player
+points = 0
 
-question1 = 
-question2 =
-question3 =
-question4 =
-question5 =
-question6 =
-question7 =
-question8 =
-question9 =
-question10 =
-question11 =
-question12 =
-question13 =
-question14 =
-question15 =
-question16 =
-question17 =
-question18 =
-question19 =
-question20 =
+Question, Answer, Answer Choices, 
+
+
+question1 = q("Which country has won the most world cups in history?", "Brazil" , ["England", "Argentina", "Germany", "Brazil"])
+question2 = q("How many times has Lionel Messi won the ballon d'or?", "6" , ["3", "6", "5", "10"]
+question3 = q("How many players are on the field for one team in a game", "11" , ["10", "3", "9", "11"]
+question4 = q("Which player is responsible for scoring goals most of the time?", "Striker" , ["Striker", "Defender", "Winger", "Attacking Midfielder"]
+question5 = q("", "answer" , ["choices", "", "", ""]
+question6 = q("question", "answer" , ["choices", "", "", ""]
+question7 = q("question", "answer" , ["choices", "", "", ""]
+question8 = q("question", "answer" , ["choices", "", "", ""]
+question9 = q("question", "answer" , ["choices", "", "", ""]
+question10 = q("question", "answer" , ["choices", "", "", ""]
+question11 = q("question", "answer" , ["choices", "", "", ""]
+question12 = q("question", "answer" , ["choices", "", "", ""]
+question13 = q("question", "answer" , ["choices", "", "", ""]
+question14 = q("question", "answer" , ["choices", "", "", ""]
+question15 = q("question", "answer" , ["choices", "", "", ""]
+question16 = q("question", "answer" , ["choices", "", "", ""]
+question17 = q("question", "answer" , ["choices", "", "", ""]
+question18 = q("question", "answer" , ["choices", "", "", ""]
+question19 = q("question", "answer" , ["choices", "", "", ""]
+question20 = q("question", "answer" , ["choices", "", "", ""]
 
 print random()
