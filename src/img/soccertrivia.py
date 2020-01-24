010ee4e6849dbde779b542fd523e884e426756a5
kodiesarkodie <58040904+kodiesarkodie@users.noreply.github.com>
2020-01-23 20:11:15 -0500

    Update soccertrivia.py

--- src/img/soccertrivia.py
+++ src/img/soccertrivia.py
@@ -7,10 +7,10 @@ username = input("What is your name? ")
 print("What's up {username}? Let's start playing the Soccer Trivia game!")
 
 
-question1 = 
-question2 =
-question3 =
-question4 =
+question1 = Q("Who has won the most World Cups in history?")
+question2 = "Who has won the most Premier Leagues in history"
+question3 = "What team has Lionel Messi played on his whole career?"
+question4 = ""
 question5 =
 question6 =
 question7 =
